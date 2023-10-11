c97197113ddecdb4a05ec789c3401d1ad87a6f73
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-10-11 17:24:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ThemeProvider theme={theme}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter basename="/project-healthy-hub">
+          <BrowserRouter basename="/fs-76-3-react-node-healthy-hub/">
             <App />
           </BrowserRouter>
         </PersistGate>
